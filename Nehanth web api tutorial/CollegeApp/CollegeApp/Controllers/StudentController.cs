f7710d11bb38b85dc597d4e0b9657eb825295c9a
sajeevamil <108211439+sajeevamil@users.noreply.github.com>
2024-07-12 15:07:49 +0530

    initial commit

---
+++ Nehanth web api tutorial/CollegeApp/CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All")]
+        public ActionResult<IEnumerable<Student>> GetStudents()
+        {
+            return Ok(CollegeRepository.Students);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Id should be greater than 0");
+            var student = CollegeRepository.Students.Where(student => student.Id == id)?.FirstOrDefault();
+            if (student is null)
+                return NotFound($"The student with {id} is not found");
+
+            return Ok(student);
+        }
+
+        [HttpGet]
+        [Route("{name:alpha}")]
+        public ActionResult<Student> GetStudentByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return BadRequest("name cannot be null");
+            var student = CollegeRepository.Students.Where(student => student.StudentName.Equals(name, StringComparison.OrdinalIgnoreCase))?.FirstOrDefault();
+            if (student is null)
+                return NotFound($"There no student with name {name}");
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<bool> DeleteStudent(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Id cannot be null");
+            var student = CollegeRepository.Students.Where(student => student.Id == id).FirstOrDefault();
+            if (student is null)
+                return NotFound($"The student with {id} is not found");
+            CollegeRepository.Students.Remove(student);
+            return Ok(true);
+        }
+    }
+}
---
+++ Nehanth web api tutorial/CollegeApp/CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static IList<Student> Students { get; } = new List<Student>()
+            {
+                new Student {
+                    Id = 1,
+                    StudentName = "Student name 1",
+                    Email = "StudentEmail1@gmail.com",
+                    Address = "Student 1 Address",
+                },
+                new Student {
+                    Id = 2,
+                    StudentName = "Student name 2",
+                    Email = "StudentEmail2@gmail.com",
+                    Address = "Student 2 Address",
+                }
+            };
+    }
+}
